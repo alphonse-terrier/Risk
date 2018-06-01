e8376cd9188f774f14dc19d270355e12e5e21e1f
aymericbdb <aymeric.deberc@gmail.com>
2018-06-01 10:12:13 +0200

    bjr

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -11,13 +11,13 @@ public class Fenetre extends JFrame {
 
     public Fenetre() {
 
-        this.setTitle("Jeu Risk par Aymeric Bès de Berc & Alphonse Terrier");
+        this.setTitle("Jeu Risk par Aymeric Bès de Berc & Alphonse Terrier"); //Titre de la fenêtre
 
-        this.setSize(1000, 690);
+        this.setSize(1000, 690); //Taille de la fenêtre
 
         this.setLocationRelativeTo(null);
 
-        this.setResizable(false);
+        this.setResizable(false); //La fenêtre n'est pas redimensionnable
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.setContentPane(new Map());
